3159da8f14eb8bf8abd928c6c02dd61a60f70271
ParkSeongHa <136056187+ParkSeongHaa@users.noreply.github.com>
2024-02-20 19:47:21 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 3 - 1 == 2
+
+def test_multiplication():
+    assert 2 * 2 == 4
+
+def test_division():
+    assert 6 / 2 == 3
